e5f2c9a3c9d28123afd81b809ddb7e8425e46739
createroftheearth <hshimanshu73@gmail.com>
2025-04-06 16:37:27 +0530

    completed

--- GST_API_Library/Services/GSTNConstants.cs
+++ GST_API_Library/Services/GSTNConstants.cs
@@ -8,15 +8,19 @@ namespace GST_API_Library.Services
 {
     public class GSTNConstants
     {
-        public static string base_url = EncryptionUtils.isProduction? "http://104.211.98.97:8002" : "https://devapi.gst.gov.in";
+        public static string base_url = EncryptionUtils.isProduction ? "https://bridge.balajimariline.in" : "https://devapi.gst.gov.in";
         public static string base_path = ".";
+
         public static string client_id = EncryptionUtils.isProduction ? "l7xx2f78b2f6f8bb4c5da3fec6e121cce1b5" : "l7xx5edefdd923ad438eb5b332a73269f812";
         public static string client_secret = EncryptionUtils.isProduction ? "c4e3dd6d243b4b7f84d8351975c06e7a" : "383dc1f4835f43ad80f80f6cf284cd7b";
-        public static string publicip = "192.168.1.25";
+        //public static string client_id = EncryptionUtils.isProduction ? "l7xx2f78b2f6f8bb4c5da3fec6e121cce1b5" : "l7xx5edefdd923ad438eb5b332a73269f812";
+        //public static string client_secret = EncryptionUtils.isProduction ? "c4e3dd6d243b4b7f84d8351975c06e7a" : "383dc1f4835f43ad80f80f6cf284cd7b";
+        public static string publicip = "104.211.98.97";
         public static string fp = "062023";
         public static string filename = "";
         public static string txn = "LAPN24235325555";
-
+        //client Id:- l7xx2f78b2f6f8bb4c5da3fec6e121cce1b5
+        //Client- Secret:- c4e3dd6d243b4b7f84d8351975c06e7a
         public static byte[] GetAppKeyBytes()
         {
             return EncryptionUtils.CreateAesKeyBC();
--- GST_APP/src/environments/environment.dev.ts
+++ GST_APP/src/environments/environment.dev.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   appRoot: 'https://localhost:7279/api',
   encryptKey: '1203199320052021',
-  encryptIV: '1203199320052021'
+  encryptIV: '1203199320052021',
 };
